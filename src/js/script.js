de564d2ccf0532f49ac6820e5542c0ba11857425
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2022-09-22 00:34:12 +0200

    question-block ready + all update

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
-$(document).ready(function () {
-  $(".slider").slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+function asctionSlidr1() {
+  $(document).ready(function () {
+    $(".arendation-block-slider").slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+    });
   });
-});
+}
+
+asctionSlidr1();
+
+function asctionSlidr2() {
+  $(document).ready(function () {
+    $(".realizacion-block-slider").slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+    });
+  });
+}
+
+asctionSlidr2();
 
 window.addEventListener("load", setAnimationHeader, onEntry);
 
@@ -39,3 +55,17 @@ let elements = document.querySelectorAll(".base-information__block-a--action");
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+function activateQuestions(){
+let questionArrow = document.querySelectorAll(".question-block__content-arrow");
+questionArrow.forEach(el =>{
+    el.addEventListener("click", ()=>{
+      el.classList.toggle("question-block__content-arrow--active")
+      let parentBlockElement = el.closest(".question-block__content-element")
+      let elementContent = parentBlockElement.querySelector(".question-block__answer-wrapper")
+       elementContent.classList.toggle("question-block__answer-wrapper--active")
+    })
+  })
+}
+
+activateQuestions();
